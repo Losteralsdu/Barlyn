e319526abec4999e4600632357c16a43acfa0979
Losteralsdu <90913603+Losteralsdu@users.noreply.github.com>
2026-08-09 12:37:35 +0200

    Initial Commit

---
+++ Barlyn/BarlynApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarlynApp.swift
+//  Barlyn
+//
+//  Created by Lucas Schirmer on 09.08.26.
+//
+
+import SwiftUI
+
+@main
+struct BarlynApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Barlyn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Barlyn
+//
+//  Created by Lucas Schirmer on 09.08.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
